0fe7c3b322b6d85e48897afb6e76914eb4f4ae77
Alexander Yustus <alexander.yustus@xored.com>
2019-10-18 16:39:13 +0700

    print several ways

--- test.go
+++ test.go
@@ -3,32 +3,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-func printStatistics() {
-	elapsed := time.Duration(3333*time.Second)
-	elapsedRunning := time.Duration(3000*time.Second)
-
-	successTests := 340
-	failedTests := 2
-	skippedTests := 44
-	timeoutTests := 3
-
-	failedNames := "\n\t\ttest1\n\t\ttest2"
-
-	logrus.Infof("Statistics:" +
-		fmt.Sprintf("\n\tElapsed total: %v", elapsed) +
-		fmt.Sprintf("\n\tTests time: %v", elapsedRunning) +
-		fmt.Sprintf("\n\tTasks  Completed: %d", 445) +
-		fmt.Sprintf("\n\tStatus  Passed: %d"+
-			"\n\tStatus  Failed: %d%v"+
-			"\n\tStatus  Timeout: %d"+
-			"\n\tStatus  Skipped: %d", successTests, failedTests, failedNames, timeoutTests, skippedTests))
-}
-
 func main() {
-	printStatistics()
+	msg := "Statistics:" +
+		"\n\tStatus  Passed: 7" +
+		"\n\tStatus  Failed: 3" +
+		"\n\t\ttest1\n\t\ttest2"
+
+	//fmt.Println("=================== via fmt.Println:")
+	//fmt.Println(msg)
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+	})
+
+	fmt.Println("=================== via logrus.Infof:")
+	logrus.Info(msg)
 }
